167c38294af048e9ef49cbf37f04444869769ce4
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-12-09 18:28:37 +0100

    Update scripts

--- scripts/import_schema.sql
+++ scripts/import_schema.sql
@@ -14,8 +14,6 @@ CREATE TABLE MyAmazingFitness (
     NPA INT NOT NULL
 );
 
-----------------------------------------------
-
 CREATE TABLE Personne (
     id SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -29,7 +27,6 @@ CREATE TABLE Personne (
     NPA INT NOT NULL,
     pays VARCHAR(255) NOT NULL
 );
-
 
 CREATE TABLE Employe (
     employe_id INT PRIMARY KEY,
@@ -57,16 +54,12 @@ CREATE TABLE Visiteur (
     visiteEffectuee BOOLEAN DEFAULT FALSE
 );
 
-----------------------------------------------
-
 CREATE TABLE Compte (
     username VARCHAR(255) PRIMARY KEY,
     motDePasse VARCHAR(255) NOT NULL,
     dateDeCreation DATE NOT NULL,
     IBAN VARCHAR(255)
 );
-
-----------------------------------------------
 
 CREATE TABLE Passage (
     passage_id INT PRIMARY KEY,
@@ -103,8 +96,6 @@ CREATE TABLE InstructeurTypeCours (
     PRIMARY KEY (instructeur_id, typeCours)
 );
 
-----------------------------------------------
-
 CREATE TABLE Salle (
     -- car il y a des lettres dans les noms de salles
     salle_id VARCHAR(255) NOT NULL,
@@ -114,7 +105,6 @@ CREATE TABLE Salle (
     PRIMARY KEY(salle_id, fitness_id)
 );
 
-
 CREATE TABLE Machine (
     machine_id INT PRIMARY KEY,
     fitness_id INT NOT NULL,
@@ -123,12 +113,9 @@ CREATE TABLE Machine (
     type_machine VARCHAR(255) NOT NULL
 );
 
-
 CREATE TABLE TypeMachine (
     type_machine VARCHAR(255) PRIMARY KEY
 );
-
-----------------------------------------------
 
 CREATE TABLE Contrat (
     contrat_id INT PRIMARY KEY,
@@ -162,8 +149,6 @@ CREATE TABLE Facture (
     PRIMARY KEY (facture_id, contrat_id)
 );
 
-----------------------------------------------
-
 CREATE TABLE Progression (
     progression_id INT NOT NULL PRIMARY KEY,
     date DATE,
@@ -171,15 +156,13 @@ CREATE TABLE Progression (
     membre_id INT NOT NULL
 );
 
-----------------------------------------------
-
 CREATE TABLE SuppressionLog (
     user_id INT,
     type VARCHAR(255),
     date_suppression TIMESTAMP
 );
 
---- Employe ---
+----------------------------------------------
 
 ALTER TABLE Employe
 ADD FOREIGN KEY (employe_id) REFERENCES Personne(id);
@@ -190,17 +173,11 @@ ADD FOREIGN KEY (fitness_id) REFERENCES MyAmazingFitness(fitness_id);
 ALTER TABLE Employe
 ADD FOREIGN KEY (compte_id) REFERENCES Compte(username);
 
---- PersonnelAdministratif ---
-
 ALTER TABLE PersonnelAdministratif
 ADD FOREIGN KEY (padministratif_id) REFERENCES Employe(employe_id);
 
---- Administratif ---
-
 ALTER TABLE Administrateur
 ADD FOREIGN KEY (administrateur_id) REFERENCES Employe(employe_id);
-
---- Membre ---
 
 ALTER TABLE Membre
 ADD FOREIGN KEY (membre_id) REFERENCES Personne(id);
@@ -208,15 +185,11 @@ ADD FOREIGN KEY (membre_id) REFERENCES Personne(id);
 ALTER TABLE Membre
 ADD FOREIGN KEY (compte_id) REFERENCES Compte(username);
 
---- Visiteur ---
-
 ALTER TABLE Visiteur
 ADD FOREIGN KEY (visiteur_id) REFERENCES Personne(id);
 
 ALTER TABLE Visiteur
 ADD FOREIGN KEY (fitness_id) REFERENCES MyAmazingFitness(fitness_id);
-
---- Passage ---
 
 ALTER TABLE Passage
 ADD FOREIGN KEY (membre_id) REFERENCES Membre(membre_id);
@@ -224,12 +197,8 @@ ADD FOREIGN KEY (membre_id) REFERENCES Membre(membre_id);
 ALTER TABLE Passage
 ADD FOREIGN KEY (fitness_id) REFERENCES MyAmazingFitness(fitness_id);
 
---- Instructeur ---
-
 ALTER TABLE Instructeur
 ADD FOREIGN KEY (instructeur_id) REFERENCES Employe(employe_id);
-
---- Cours ---
 
 ALTER TABLE Cours
 ADD FOREIGN KEY (instructeur_id) REFERENCES Instructeur(instructeur_id);
@@ -243,20 +212,14 @@ ADD FOREIGN KEY (fitness_id, salle_id) REFERENCES Salle(fitness_id, salle_id);
 ALTER TABLE Cours
 ADD FOREIGN KEY (abo_id) REFERENCES Abonnement(abo_id);
 
---- InstructeurTypeCours ---
-
 ALTER TABLE InstructeurTypeCours
 ADD FOREIGN KEY (instructeur_id) REFERENCES Instructeur(instructeur_id);
 
 ALTER TABLE InstructeurTypeCours
 ADD FOREIGN KEY (typeCours) REFERENCES TypeCours(nom);
 
---- Salle ---
-
 ALTER TABLE Salle
 ADD FOREIGN KEY (fitness_id) REFERENCES MyAmazingFitness(fitness_id);
-
---- Machine ---
 
 ALTER TABLE Machine
 ADD FOREIGN KEY (fitness_id, salle_id) REFERENCES Salle(fitness_id, salle_id);
@@ -264,12 +227,8 @@ ADD FOREIGN KEY (fitness_id, salle_id) REFERENCES Salle(fitness_id, salle_id);
 ALTER TABLE Machine
 ADD FOREIGN KEY (type_machine) REFERENCES TypeMachine(type_machine);
 
---- Contrat ---
-
 ALTER TABLE Contrat
 ADD FOREIGN KEY (membre_id) REFERENCES Membre(membre_id);
-
---- ContratAbonnement ---
 
 ALTER TABLE ContratAbonnement
 ADD FOREIGN KEY (contrat_id) REFERENCES Contrat(contrat_id);
@@ -277,12 +236,8 @@ ADD FOREIGN KEY (contrat_id) REFERENCES Contrat(contrat_id);
 ALTER TABLE ContratAbonnement
 ADD FOREIGN KEY (abo_id) REFERENCES Abonnement(abo_id);
 
---- Facture ---
-
 ALTER TABLE Facture
 ADD FOREIGN KEY (contrat_id) REFERENCES Contrat(contrat_id);
-
---- Progression ---
 
 ALTER TABLE Progression
 ADD FOREIGN KEY (membre_id) REFERENCES Membre(membre_id);
