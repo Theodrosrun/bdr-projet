72e8e2c4dcab77f7136dac4e1c72da899f33c746
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-12-11 13:57:16 +0100

    Adding request

---
+++ scripts/request.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM Membre WHERE nom = 'Doe';
+
+SELECT * FROM Employe WHERE salaire > 5000;
+
+SELECT * FROM Cours WHERE jour = 'Lundi';
+
+SELECT * FROM Abonnement WHERE prix < 50;
+
+SELECT * FROM facture WHERE montant > 50;
