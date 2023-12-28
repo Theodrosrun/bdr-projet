8e9af950076729b4c576eb1993098cfdb54d4073
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-12-28 13:55:02 +0100

    Update

--- src/main/java/ch/heigvd/components/Title.java
+++ src/main/java/ch/heigvd/components/Title.java
@@ -1,23 +1,24 @@
 package ch.heigvd.components;
 
+import ch.heigvd.utils.freemarker.FreeMarkerConfig;
+import freemarker.template.Template;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Title {
     public static String doGet(String title) {
-        return """
-                    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb/classes-breadcrumb.jpg">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="breadcrumb-text">
-                                        <h2>%s</h2>
-                                        <div class="breadcrumb-option">
-                                            <a href="./home"><i class="fa fa-home"></i> Home</a>
-                                            <span>%s</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                """.formatted(title, title);
+        try {
+            Map<String, Object> data = new HashMap<>();
+            data.put("title", title);
+
+            Template template = FreeMarkerConfig.getConfig().getTemplate("title.ftlh");
+            StringWriter out = new StringWriter();
+            template.process(data, out);
+            return out.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erreur lors de la génération du contenu : " + e.getMessage();
+        }
     }
 }
